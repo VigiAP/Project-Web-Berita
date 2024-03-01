6eb4d857c1ff5ca0a6a79403cb36dda41261d687
VigiAP <vigi.ap@unsub.ac.id>
2024-03-01 18:13:32 +0700

    update tailwind

---
+++ app/Views/Home/index.php
@@ -0,0 +1,6 @@
+<?= $this->extend('/template/t_home'); ?>
+<?= $this->section('konten'); ?>
+
+
+
+<?= $this->endSection(); ?>
---
+++ app/Views/template/t_home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="<?= base_url('css/styles.css'); ?>">
+</head>
+
+<body>
+
+    <!-- navbar -->
+
+    <nav>
+        <div>
+            <img src="" alt="">
+        </div>
+    </nav>
+
+    <!-- navbar end -->
+
+
+    <?= $this->renderSection('konten'); ?>
+
+
+</body>
+
+</html>
