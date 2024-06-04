3ea79f53ad99dbaf0bf4b5d8daf254634ab40c75
VigiAP <vigi.ap@unsub.ac.id>
2024-06-04 15:42:20 +0700

    d

--- app/Views/Home/category.php
+++ app/Views/Home/category.php
@@ -15,25 +15,6 @@
               </h2>
             </div>
             <div class="flex flex-row flex-wrap -mx-3">
-              <div class="flex-shrink max-w-full w-full px-3 pb-5">
-                <div class="relative hover-img max-h-98 overflow-hidden">
-                  <!--thumbnail-->
-                  <a href="#">
-                    <img class="max-w-full w-full mx-auto h-auto" src="src/img/dummy/img22.jpg" alt="Image description">
-                  </a>
-                  <div class="absolute px-5 pt-8 pb-5 bottom-0 w-full bg-gradient-cover">
-                    <!--title-->
-                    <a href="#">
-                      <h2 class="text-3xl font-bold capitalize text-white mb-3">Amazon Shoppers Are Ditching Designer Belts for This Best-Selling</h2>
-                    </a>
-                    <p class="text-gray-100 hidden sm:inline-block">Russia says a fire on its flagship vessel in the Black Sea detonated munitions - Ukraine says it fired missiles at the Moskva</p>                                                  
-                    <!-- author and date -->
-                    <div class="pt-2">
-                      <div class="text-gray-100"><div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>Europe</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
 
               <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
                 <div class="flex flex-row sm:block hover-img">
@@ -45,7 +26,7 @@
                       <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
                     </h3>
                     <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
+                    <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>kategori</a>
                   </div>
                 </div>
               </div>
