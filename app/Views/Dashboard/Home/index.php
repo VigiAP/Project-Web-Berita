60200edb49f0e94c8d25d54cf2fbb55b5485ef4f
VigiAP <vigi.ap@unsub.ac.id>
2024-06-07 13:44:09 +0700

    update

--- app/Views/Dashboard/Home/index.php
+++ app/Views/Dashboard/Home/index.php
@@ -2,9 +2,7 @@
 <?= $this->section('konten'); ?>
 <section class="content">
     <div class="container-fluid">
-        <div class="card-body">
-            <h1></h1>SELAMAT DATANG DI HALAMAN <?=ucwords(session()->get('name'))?></h1>
-        </div>
+        
     </div><!-- /.container-fluid -->
 </section>
 <?= $this->endSection(); ?>
