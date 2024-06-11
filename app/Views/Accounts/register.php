b9a89feb6d4a9cf1421236daca45cc12aaaff682
VigiAP <vigi.ap@unsub.ac.id>
2024-06-11 18:46:24 +0700

    ds

--- app/Views/Accounts/register.php
+++ app/Views/Accounts/register.php
@@ -22,6 +22,31 @@
                     </label>
                     <input type="password" placeholder="Password" class="w-full input input-bordered" name="password" />
                 </div>
+                <div>
+                <label class="label -mt-2">
+                        <span class="text-base label-text">Gender</span>
+                    </label>
+                    <div class="flex space-x-4">
+                        <div>
+                            <label class="label cursor-pointer">
+                                <input type="radio" name="gender" value="male" class="radio radio-primary" checked>
+                                <span class="label-text ml-2">Male</span>
+                            </label>
+                        </div>
+                        <div>
+                            <label class="label cursor-pointer">
+                                <input type="radio" name="gender" value="female" class="radio radio-primary">
+                                <span class="label-text ml-2">Female</span>
+                            </label>
+                        </div>
+                        <div>
+                            <label class="label cursor-pointer">
+                                <input type="radio" name="gender" value="unspecified" class="radio radio-primary">
+                                <span class="label-text ml-2">Prefer not to say</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
                 <div class="mt-5">
                     <button class="btn btn-block border border-gray-400">Register</button>
                     <a href="<?= base_url('Accounts/'); ?>" class="mt-3 btn btn-block border border-gray-400">Back</a>
--- app/Views/Home/Hasil_Search.php
+++ app/Views/Home/Hasil_Search.php
@@ -2,17 +2,7 @@
 <?= $this->section('konten'); ?>
 
 <main id="content">
-    <!-- advertisement -->
-    <div class="bg-gray-50 py-4 hidden">
-        <div class="xl:container mx-auto px-3 sm:px-4 xl:px-2">
-            <div class="mx-auto table text-center text-sm">
-                <a class="uppercase" href="#">Advertisement</a>
-                <a href="#">
-                    <img src="src/img/ads/ads_728.jpg" alt="advertisement area">
-                </a>
-            </div>
-        </div>
-    </div>
+
 
     <!-- block news -->
     <div class="bg-gray-50 py-6">
@@ -49,38 +39,7 @@
                         </div>
                     </div>
                 </div>
-                <!-- right -->
-                <div class="flex-shrink max-w-full w-full lg:w-1/3 lg:pl-8 lg:pt-14 lg:pb-8 order-last lg:order-last lg:order-first">
-                    <div class="w-full bg-white">
-                        <div class="mb-6">
-                            <div class="p-4 bg-gray-100">
-                                <h2 class="text-lg font-bold">Most Popular</h2>
-                            </div>
-                            <ul class="post-number">
-                                <li class="border-b border-gray-100 hover:bg-gray-50">
-                                    <a class="text-lg font-bold px-6 py-3 flex flex-row items-center" href="#">Why the
-                                        world would end without political polls</a>
-                                </li>
-                                <li class="border-b border-gray-100 hover:bg-gray-50">
-                                    <a class="text-lg font-bold px-6 py-3 flex flex-row items-center" href="#">Meet The
-                                        Man Who Designed The Ducati Monster</a>
-                                </li>
-                                <li class="border-b border-gray-100 hover:bg-gray-50">
-                                    <a class="text-lg font-bold px-6 py-3 flex flex-row items-center" href="#">2020 Audi
-                                        R8 Spyder spy shots release</a>
-                                </li>
-                                <li class="border-b border-gray-100 hover:bg-gray-50">
-                                    <a class="text-lg font-bold px-6 py-3 flex flex-row items-center"
-                                        href="#">Lamborghini makes Huracán GT3 racer faster for 2019</a>
-                                </li>
-                                <li class="border-b border-gray-100 hover:bg-gray-50">
-                                    <a class="text-lg font-bold px-6 py-3 flex flex-row items-center" href="#">ZF plans
-                                        $14 billion autonomous vehicle push, concept van</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
