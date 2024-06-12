80ad5de42260a12b49f91eefd8896a37a68dcb28
VigiAP <vigi.ap@unsub.ac.id>
2024-06-12 13:54:07 +0700

    update

--- app/Views/template/sidebarMenuAuthor.php
+++ app/Views/template/sidebarMenuAuthor.php
@@ -7,14 +7,6 @@
     </a>
 </li>
 <li class="nav-item">
-    <a href="<?= base_url('Author/comments'); ?>" id="sidebar-nav" class="nav-link" style="border: 1px solid gray;">
-    <i class="fa-solid fa-comment ml-1"></i>
-        <p>
-            Comment
-        </p>
-    </a>
-</li>
-<li class="nav-item">
     <a href="<?= base_url('Author/category'); ?>" id="sidebar-nav" class="nav-link" style="border: 1px solid gray;">
     <i class="fa-solid fa-list"></i>
         <p>
