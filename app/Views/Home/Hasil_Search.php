d08020d5209c803ef57487107449451867329e33
VigiAP <vigi.ap@unsub.ac.id>
2024-06-18 21:10:14 +0700

    s

--- app/Views/Home/Hasil_Search.php
+++ app/Views/Home/Hasil_Search.php
@@ -6,7 +6,7 @@
         <div class="xl:container mx-auto px-3 sm:px-4 xl:px-2">
             <div class="flex flex-col lg:flex-row flex-wrap">
                 <div class="flex-shrink max-w-full w-full lg:w-2/3 overflow-hidden">
-                    <div class="flex flex-row flex-wrap -mx-3">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 -mx-3">
                         <div class="flex-shrink max-w-full w-full px-3">
                             <div class="w-full py-3 mb-4">
                                 <h2 class="text-gray-800 text-3xl font-bold">
@@ -15,7 +15,7 @@
                             </div>
                         </div>
                         <?php foreach ($articles as $article): ?>
-                        <div class="flex-shrink max-w-full w-full sm:w-1/2 md:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
+                        <div class="flex-shrink max-w-full w-full px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
                             <div class="flex flex-row sm:block hover-img">
                                 <a href="<?= base_url('Home/singlePost/' . $article['id_article']); ?>">
                                     <img class="max-w-full w-full mx-auto" src="<?= base_url('img/' . $article['image']); ?>" alt="<?= $article['title']; ?>">
